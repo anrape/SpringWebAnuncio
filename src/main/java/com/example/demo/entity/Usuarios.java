d8565d18200199c53236aca5bd306f3d7e2220ed
sergiotarxz <sergiotargz@openmailbox.org>
2017-11-14 14:07:09 +0100

    Rompiendo cosas

--- src/main/java/com/example/demo/entity/Usuarios.java
+++ src/main/java/com/example/demo/entity/Usuarios.java
@@ -9,71 +9,95 @@ import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.MessageDigest;
 
 @Entity
-@Table(name = "Usuario")
+@Table(name = "usuarios")
 public class Usuarios {
 
-	@Id
-	@Column(name = "nombre", unique = true, nullable = false, length = 10)
-	private String usuario;
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "nombre", unique = true, nullable = false, length = 10)
+    private String usuario;
 
-	@Column(name = "contraseña", nullable = false, length = 15)
-	private String contraseña;
+    @Column(name = "contraseña", nullable = false, length = 65)
+    private String contrasena;
 
-	@Column(name = "estado", nullable = false)
-	private boolean estado;
+    @Column(name = "estado", nullable = false)
+    private boolean estado;
 
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
-	private Set<Rol> rol = new HashSet<Rol>();
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
+    private Set<Rol> rol = new HashSet<Rol>();
 
-	public Usuarios(String usuario, String contraseña, Boolean estado) {
-		super();
-		this.usuario = usuario;
-		this.contraseña = contraseña;
-		this.estado = estado;
-	}
+    public Usuarios(String usuario, String contrasena, Boolean estado) {
+	super();
+	this.usuario = usuario;
+	this.contrasena = Usuarios.sha256(contrasena);
+	this.estado = estado;
+    }
 	
-	public Usuarios(String usuario, String contraseña, Boolean estado, Set<Rol> rol) {
-		super();
-		this.usuario = usuario;
-		this.contraseña = contraseña;
-		this.estado = estado;
-		this.rol = rol;
-	}
+    public Usuarios(String usuario, String contrasena, Boolean estado, Set<Rol> rol) {
+	super();
+	this.usuario = usuario;
+	this.contrasena = Usuarios.sha256(contrasena);
+	this.estado = estado;
+	this.rol = rol;
+    }
 
-	public String getUsuario() {
-		return usuario;
-	}
+    public String getUsuario() {
+	return usuario;
+    }
 
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
-	}
+    public void setUsuario(String usuario) {
+	this.usuario = usuario;
+    }
 
-	public String getContraseña() {
-		return contraseña;
-	}
+    public String getContrasena() {
+	return contrasena;
+    }
 
-	public void setContraseña(String contraseña) {
-		this.contraseña = contraseña;
-	}
-
-	public boolean isEstado() {
-		return estado;
-	}
-
-	public void setEstado(boolean estado) {
-		this.estado = estado;
-	}
-
-	public Set<Rol> getUserRol() {
-		return rol;
-	}
-	public void setUserRole(Set<Rol> rol) {
-		this.rol = rol;
-	}
+    public void setContrasena(String contrasena) {
 	
-	
-	
+	this.contrasena = Usuarios.sha256(contrasena);
+    }
 
+    public boolean isEstado() {
+	return estado;
+    }
+
+    public void setEstado(boolean estado) {
+	this.estado = estado;
+    }
+
+    public Set<Rol> getUserRol() {
+	return rol;
+    }
+    public void setUserRole(Set<Rol> rol) {
+	this.rol = rol;
+    }	
+    public static String sha256(String base) {
+	try {
+	    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	    byte[] hash = digest.digest(base.getBytes("UTF-8"));
+	    StringBuffer hexString = new StringBuffer();
+
+	    for (int i = 0; i < hash.length; i++) {
+		String hex = Integer.toHexString(0xff & hash[i]);
+		if (hex.length() == 1) {
+		    hexString.append('0');
+		}
+		hexString.append(hex);
+	    }
+
+	    return hexString.toString();
+	} catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
+	    throw new RuntimeException(ex);
+	}
+    }
+    
 }
