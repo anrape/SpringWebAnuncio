79f5fafb7ca4d5e8a6f570210f48364a8b8fd935
sergiotarxz <sergiotargz@openmailbox.org>
2017-11-27 14:09:40 +0100

    actividad hoy

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,3 +1,4 @@
+
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
@@ -6,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/index")
 public class IndexController {
     public static final String INDEX_VIEW = "index";
 
